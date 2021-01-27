31aeeb1f467b205111851e34a0beb03dc120deb6
Dejan Trtnik <46469418+dejantrtnik@users.noreply.github.com>
2021-01-27 15:38:21 +0100

    First release

---
+++ Bootstrap/css_js_jquery.php
@@ -0,0 +1,22 @@
+<?php
+namespace css_js_jquery;
+
+class Css {
+  const BOOTSTRAP = '<link rel="stylesheet" href="https://www.spletniprojekti.eu/const/assets/css/bootstrap.css">';
+  const BOOTSTRAP_MIN = '<link rel="stylesheet" href="https://www.spletniprojekti.eu/const/assets/css/bootstrap.min.css">';
+}
+
+class Js
+{
+  const BOOTSTRAP_JS = '<script src="https://www.spletniprojekti.eu/const/assets/js/bootstrap.js"></script>';
+  const BOOTSTRAP_MIN_JS = '<script src="https://www.spletniprojekti.eu/const/assets/js/bootstrap.min.js"></script>';
+
+  const POPPER_MIN_JS = '<script src="https://www.spletniprojekti.eu/const/assets/js/popper.min.js"></script>';
+}
+
+class jquery
+{
+  const JQUERY_SLIM_MIN = '<script src="https://www.spletniprojekti.eu/const/assets/js/jquery-3.5.1.min.js"></script>';
+}
+
+?>
