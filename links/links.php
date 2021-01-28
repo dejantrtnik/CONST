de792ed1c98d53cd58c91fd938c65ac918cfcfa7
Dejan Trtnik <46469418+dejantrtnik@users.noreply.github.com>
2021-01-28 12:08:31 +0100

    Add some stuff

---
+++ links/links.php
@@ -0,0 +1,22 @@
+<?php
+namespace links;
+defined('BASEPATH') or exit('No direct script access allowed');
+
+/**
+ *
+ */
+class PermLinks
+{
+  const home = 'http://www.spletniprojekti.eu/';
+  const spl = 'https://www.spletniprojekti.eu/spl/';
+
+}
+
+class PermLinksImg
+{
+  const logoTitle = '<link rel="shortcut icon" type="image/gif/png" href="https://www.spletniprojekti.eu/const/img/logo_server_black.png">';
+  const logoLink  = '<a href="http://www.spletniprojekti.eu/"><img src="https://www.spletniprojekti.eu/const/img/logo_server_black_small.png" alt=""></a>';
+
+}
+
+?>
